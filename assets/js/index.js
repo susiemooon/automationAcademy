766b0ae5a3bdcd835784f3904399bbb96a77784b
Helena <missalena99@outlook.com>
2019-07-29 18:01:04 +0300

    another changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,38 +7,38 @@ var companies = document.getElementById('img-companies');
 companies.src = imgC;
 var slideIndex = 0;
 var dotsIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+var dots1 = document.getElementsByClassName("dot1");
+
 showSlides();
 
 
 function showSlides() {
-    var i;
+  var i;
 
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    var dots1 = document.getElementsByClassName("dot1");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-        
-    }
-    slideIndex++;
-    
-    if (slideIndex > slides.length/2) {slideIndex = 1} 
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    for (i = 0; i < dots1.length; i++) {
-      dots1[i].className = dots1[i].className.replace(" active1", "");
-    }
-    slides[slideIndex-1].style.display = "block"; 
-    slides[slideIndex-1+(slides.length/2)].style.display = "block";
-    dots[dotsIndex].className += " active";
-    dots1[dotsIndex].className += " active1";
-    
-    dotsIndex+=4;
-    if(dotsIndex>dots.length)
-    dotsIndex=0;  
-            
-    setTimeout(showSlides, 4000); 
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length / 2) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  for (i = 0; i < dots1.length; i++) {
+    dots1[i].className = dots1[i].className.replace(" active1", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1 + (slides.length / 2)].style.display = "block";
+  dots[dotsIndex].className += " active";
+  dots1[dotsIndex].className += " active1";
+
+  dotsIndex += 4;
+  if (dotsIndex > dots.length)
+    dotsIndex = 0;
+
+  setTimeout(showSlides, 4000);
 }
 
-//document.body.appendChild(component());
